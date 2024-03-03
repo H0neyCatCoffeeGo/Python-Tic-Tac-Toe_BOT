f355ba13adc373440fec7d8e036aa568cc62ba77
Wahab <129193641+H0neyCatCoffeeGo@users.noreply.github.com>
2024-03-03 18:50:57 +0500

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.pygame
+      python-pkgs.requests
+    ]))
+  ];
+shellHook = ''
+  echo -e "C++ Shell Entered"
+'';
+}
+
+
